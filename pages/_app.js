9a8f7df7ac95a66c27b628107fc2ff6b6f650168
mey2023 <it.reksmey@gmail.com>
2023-05-02 12:06:24 +0700

    movies app

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import '@/styles/globals.css'
-
+import '@/styles/bootstrap.min.css'
+import Script from 'next/script'
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" />
+    <Component {...pageProps} />
+    </>
+  )
 }
